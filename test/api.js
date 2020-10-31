42f2b66eacee918aecb27e929b1bbc87a470a68c
Henry Liu <liuhenry64@gmail.com>
2020-10-30 23:23:34 -0400

    Progress project2

--- test/api.js
+++ test/api.js
@@ -1,4 +1,11 @@
 export const fetchMovies = async () => {
     const response = await fetch('http://omdbapi.com/?apikey=4c3128ae&s&t=21');
-    
+    const {results} = await response.json();
+    return results.map(formatMovies);
 }
+
+const formatMovies = movie => ({
+    // Figure out json response format
+    Title = movie.title;
+    Year = ;
+})
